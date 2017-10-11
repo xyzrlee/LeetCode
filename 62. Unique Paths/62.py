b54f7d28f95d41f78eb250c00d115f7bbd1dcd09
xyzrlee <cnrickylee@gmail.com>
2017-10-11 10:09:09 +0800

    62

---
+++ 62. Unique Paths/62.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        rslt = []
+        for i in range(m):
+            r = []
+            for j in range(n):
+                if i == 0:
+                    r.append(1)
+                elif j == 0:
+                    r.append(1)
+                else:
+                    r.append(r[j - 1] + rslt[i - 1][j])
+            rslt.append(r)
+        return rslt[m-1][n-1]
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.uniquePaths(3, 7))
