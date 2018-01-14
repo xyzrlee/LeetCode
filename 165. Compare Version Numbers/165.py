2f6a351a737a94db8c4b28af4e40c88038a9bcfe
Ricky Li <cnrickylee@gmail.com>
2018-01-14 14:52:06 +0800

    165

--- 165. Compare Version Numbers/165.py
+++ 165. Compare Version Numbers/165.py
@@ -9,25 +9,19 @@ class Solution(object):
         v2 = version2.split('.')
         len1 = len(v1)
         len2 = len(v2)
-        print(len1, len2)
-        for i in range(min(len1, len2)):
-            if v1[i] != v2[i]:
-                i1 = int(v1[i])
-                i2 = int(v2[i])
-                if i1 == i2:
-                    continue
-                if i1 < i2:
-                    return -1
-                else:
-                    return 1
-        if len1 > len2:
-            return 1
-        elif len1 < len2:
-            return -1
-        else:
-            return 0
+        l = max(len1, len2)
+        for i in range(l):
+            i1 = 0 if i >= len1 else int(v1[i])
+            i2 = 0 if i >= len2 else int(v2[i])
+            if i1 == i2:
+                continue
+            if i1 < i2:
+                return -1
+            else:
+                return 1
+        return 0
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.compareVersion('01', '1'))
+    print(sol.compareVersion('01.0', '1'))
